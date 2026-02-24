522b6f90490db223971905dcd5549c67fbe5913c
AndresB <andres_bs@hotmail.com>
2026-02-24 19:28:06 +0100

    Transfer update with tests

--- src/ex2/WalletService.java
+++ src/ex2/WalletService.java
@@ -1,22 +1,25 @@
 package ex2;
 
+import java.util.SortedMap;
+
 public class WalletService {
 
-    void deposit(Wallet wallet, double amount) {
-        if (wallet == null) return;
+    boolean deposit(Wallet wallet, double amount) {
+        if (wallet == null) return false;
 
         if (amount <= 0) {
             System.out.println("The amount must to be greater than 0");
-            return;
+            return false;
         }
         if (wallet.getOwner() == null || wallet.getOwner().isEmpty()) {
             System.out.println("The receiver does not exist");
-            return;
+            return false;
         }
 
 
         wallet.setBalance(wallet.getBalance() + amount);
         wallet.getHistory().add("Deposit: " + amount);
+        return true;
     }
 
     boolean withdraw(Wallet wallet, double amount) {
@@ -31,5 +34,27 @@ public class WalletService {
         return true;
 
     }
+
+    boolean transfer(Wallet senderWallet, Wallet reciverWallet, double amount) {
+        if (senderWallet == null || reciverWallet == null || senderWallet == reciverWallet) {
+            System.out.println("Check the data again");
+            return false;
+        }
+
+        if (senderWallet.getOwner() == null || senderWallet.getOwner().isEmpty() ||
+                reciverWallet.getOwner() == null || reciverWallet.getOwner().isEmpty() || amount <= 0) {
+            return false;
+        }
+
+        if (withdraw(senderWallet, amount)) {
+            deposit(reciverWallet, amount);
+            senderWallet.getHistory().add("Transfer sent to " + reciverWallet.getOwner());
+            reciverWallet.getHistory().add("Transfer received from " + senderWallet.getOwner());
+            return true;
+        }
+        return false;
+    }
+
 }
+
 
--- test/ex2/WalletServiceTest.java
+++ test/ex2/WalletServiceTest.java
@@ -103,4 +103,126 @@ public class WalletServiceTest {
         assertTrue(wallet.getHistory().isEmpty());
     }
 
+    // Transfer tests
+    @Test
+    void shouldReturnNullWhenSenderWalletIsNull() {
+        Wallet senderWallet = new Wallet(null);
+        Wallet receiverWaller = new Wallet("Andres");
+        WalletService service = new WalletService();
+
+        boolean result = service.transfer(senderWallet, receiverWaller, 100);
+        assertFalse(result, "Should not send money when senderWallet is null");
+    }
+
+    @Test
+    void shouldReturnNullWhenReceiverWalletIsNull() {
+        Wallet senderWallet = new Wallet("Andres");
+        Wallet receiverWaller = new Wallet(null);
+        WalletService service = new WalletService();
+
+        boolean result = service.transfer(senderWallet, receiverWaller, 100);
+        assertFalse(result, "Should not send money when senderWallet is null");
+    }
+
+    @Test
+    void shouldRetornFalseWhenSendingToSameAccount () {
+        Wallet senderWallet = new Wallet("Andres");
+        WalletService service = new WalletService();
+
+        boolean result = service.transfer(senderWallet, senderWallet, 100);
+        assertFalse(result, "Can not send money to same bank account.");
+    }
+
+    @Test
+    void shouldReturnFalseWhenAmountToTransferIsLessThanBalance() {
+        Wallet senderWallet = new Wallet("Andres");
+        Wallet receiverWaller = new Wallet("Juan");
+        WalletService service = new WalletService();
+
+        senderWallet.setBalance(100.00);
+
+        boolean result = service.transfer(senderWallet, receiverWaller, 101);
+        assertFalse(result, "insufficient balance");
+    }
+
+    @Test
+    void shouldReturnFalseWhenAmountToTransferIsBelowZero() {
+        Wallet senderWallet = new Wallet("Andres");
+        Wallet receiverWaller = new Wallet("Juan");
+        WalletService service = new WalletService();
+
+        senderWallet.setBalance(10);
+
+        boolean result = service.transfer(senderWallet, receiverWaller, -101);
+        assertFalse(result, "Amount below zero is not valid");
+    }
+
+    @Test
+    void shouldReturnFalseIfSenderWalletIsFalse() {
+        Wallet receiverWaller = new Wallet("Juan");
+        WalletService service = new WalletService();
+        boolean result = service.transfer(null, receiverWaller, 10);
+        assertFalse(result, "Sender or receiver can not be null");
+    }
+
+    @Test
+    void shouldReturnFalseIfReceiverWalletIsFalse() {
+        Wallet senderWaller = new Wallet("Juan");
+        WalletService service = new WalletService();
+        boolean result = service.transfer(senderWaller, null, 10);
+        assertFalse(result, "Sender or receiver can not be null");
+    }
+
+    @Test
+    void shouldHandleFailureDuringDepositInTransfer() {
+        Wallet sender = new Wallet("Andres");
+        sender.setBalance(100.0);
+
+        Wallet receiver = new Wallet(null);
+
+        WalletService service = new WalletService();
+
+
+        boolean result = service.transfer(sender, receiver, 50.0);
+
+        assertFalse(result, "The transfer will fail if the deposit is not possible");
+    }
+
+    @Test
+    void ShouldReturnFalseIfSenderWalletOwnerIsEmpty() {
+        Wallet receiverWallet = new Wallet("Juan");
+        Wallet senderWallet = new Wallet("");
+        WalletService service = new WalletService();
+
+        boolean result = service.transfer( senderWallet, receiverWallet, 10);
+        assertFalse(result, "The sender wallet name should not be null.");
+    }
+
+    @Test
+    void shouldReturnFalseIfReceiverOwnerIsEmpty() {
+        Wallet sender = new Wallet("Andres");
+        sender.setBalance(100.0);
+        Wallet receiver = new Wallet("");
+        WalletService service = new WalletService();
+        boolean result = service.transfer(sender, receiver, 50.0);
+        assertFalse(result, "Should fail because receptor has no name");
+    }
+
+    @Test
+    void ShouldTransferMoneySuccessfully() {
+        Wallet senderWallet = new Wallet("Andres");
+        Wallet receiverWaller = new Wallet("Juan");
+        WalletService service = new WalletService();
+
+        senderWallet.setBalance(10);
+        receiverWaller.setBalance(20);
+
+        boolean result = service.transfer(senderWallet, receiverWaller, 10);
+
+        assertTrue(result, "Money sent properly");
+        assertEquals(30, receiverWaller.getBalance(), "should add 10 to receiver wallet, 10 + 20(actual) = 30");
+        assertEquals(0, senderWallet.getBalance(), "Sender wallet should update to 0. 10 - 10 = 0");
+        assertFalse(senderWallet.getHistory().isEmpty(), "The sender wallet history is updated");
+        assertFalse(receiverWaller.getHistory().isEmpty(), "The receiver wallet history is updated");
+    }
 }
